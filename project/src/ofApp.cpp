06db0b93de46e1952b9d446741cab01044e34eda
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-12-08 12:20:34 +0100

    remove opencv example

--- project/src/ofApp.cpp
+++ project/src/ofApp.cpp
@@ -1,81 +1,71 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-    vidGrabber.setVerbose(true);
-    vidGrabber.setup(640, 480);
-
-    colorImg.allocate(640, 480);
-    grayImg.allocate(640, 480);
-}
-
-//--------------------------------------------------------------
-void ofApp::update(){
-    vidGrabber.update();
-
-    if (vidGrabber.isFrameNew()) {
-        colorImg.setFromPixels(vidGrabber.getPixels());
-        grayImg = colorImg;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-    colorImg.draw(0, 0);
-    grayImg.draw(640, 0);
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::setup() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::draw() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
