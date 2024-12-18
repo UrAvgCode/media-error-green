eba415ada9c82105dc3626855c0599d8665367dd
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-12-18 22:25:20 +0100

    create intro and core project

---
+++ intro/src/main.cpp
@@ -0,0 +1,14 @@
+#include "ofApp.h"
+#include "ofMain.h"
+
+//========================================================================
+int main() {
+    ofGLWindowSettings settings;
+    settings.setSize(1024, 768);
+    settings.windowMode = OF_WINDOW;
+
+    auto window = ofCreateWindow(settings);
+
+    ofRunApp(window, make_shared<ofApp>());
+    ofRunMainLoop();
+}
