84b944bce29349f12a23cd6503da85f571a416dc
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2025-01-12 19:38:21 +0100

    include vector in tracking_app.cpp

--- tracking/src/tracking_app.cpp
+++ tracking/src/tracking_app.cpp
@@ -1,5 +1,7 @@
 #include "tracking_app.h"
 
+#include <vector>
+
 //--------------------------------------------------------------
 void TrackingApp::setup() {
     // ofSetLogLevel(OF_LOG_VERBOSE);
