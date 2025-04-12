9e6bd8898b413b64eb46af1ac6627ac538aa96b5
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-04-12 15:47:51 +0200

    add collision object sounds

--- tracking/src/collision_object.cpp
+++ tracking/src/collision_object.cpp
@@ -4,9 +4,9 @@
 #include <string>
 #include <vector>
 
-#include "ofAppRunner.h"
-#include "ofFbo.h"
-
+#include <ofAppRunner.h>
+#include <ofFbo.h>
+#include <ofMath.h>
 
 CollisionObject::CollisionObject() : CollisionObject({0, 0}, {0, 0}, "", std::make_shared<EffectShader>()) {}
 
@@ -14,14 +14,28 @@ CollisionObject::CollisionObject(glm::vec2 position, glm::vec2 velocity, const s
                                  std::shared_ptr<EffectShader> effect_shader) :
     position(position), velocity(velocity), can_collide(false), effect_shader(effect_shader) {
     image.load(filename);
+
+    pluck_b.load("resources/audio/gruen_pluck_b.wav");
+    pluck_d.load("resources/audio/gruen_pluck_d.wav");
+    pluck_e.load("resources/audio/gruen_pluck_e.wav");
+    pluck_g.load("resources/audio/gruen_pluck_g.wav");
+    globalEffect.load("resources/audio/gruen_globalEffect.wav");
+
+    pluck_b.setMultiPlay(true);
+    pluck_d.setMultiPlay(true);
+    pluck_e.setMultiPlay(true);
+    pluck_g.setMultiPlay(true);
+    globalEffect.setMultiPlay(false);
 }
 
 void CollisionObject::update(std::vector<Player> &players, const ofEasyCam &camera) {
     if (position.x <= 0 || position.x + width() >= ofGetWidth()) {
+        play_random_pluck();
         velocity.x *= -1;
     }
 
     if (position.y <= 0 || position.y + height() >= ofGetHeight()) {
+        play_random_pluck();
         velocity.y *= -1;
     }
 
@@ -41,6 +55,26 @@ void CollisionObject::draw() const {
     effect_shader->begin();
     image.draw(position.x, position.y, 0);
     effect_shader->end();
+}
+
+void CollisionObject::play_random_pluck() {
+    int random = std::round(ofRandom(0, 4));
+    switch (random) {
+        case 0:
+            pluck_b.play();
+            break;
+        case 1:
+            pluck_d.play();
+            break;
+        case 2:
+            pluck_e.play();
+            break;
+        case 3:
+            pluck_g.play();
+            break;
+        default:
+            break;
+    }
 }
 
 bool CollisionObject::check_collision_with_bodies(std::vector<Player> &players, const ofEasyCam &camera) const {
