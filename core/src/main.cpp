c48cdf0f6508cd36250186917940d464ee0dd2f9
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2025-05-15 09:56:05 +0200

    enable game mode

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -9,7 +9,7 @@ int main() {
     auto settings = ofGLWindowSettings();
     settings.setSize(1920, 1080);
     settings.setGLVersion(4, 6);
-    settings.windowMode = OF_WINDOW;
+    settings.windowMode = OF_GAME_MODE;
 
     const auto window = ofCreateWindow(settings);
     ofRunApp(window, make_shared<CoreApp>());
